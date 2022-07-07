d2d918e37e5377e27f18e7ac9b7d6b7b8e8cdd96
Leandro Amorim <leandroamorimpb@gmail.com>
2022-07-07 12:59:36 -0300

    Configurando o pom.xml e criando a classe de teste com junit :package:

---
+++ envioemail/src/test/java/envioemail/email/AppTest.java
@@ -0,0 +1,12 @@
+package envioemail.email;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void testeEmail() {
+		
+		/*Olhe as configurações smtp do seu e-mail, cada tipo de e-mail de uma config*/
+	}
+}
