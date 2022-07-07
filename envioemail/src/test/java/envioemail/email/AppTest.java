c3ffe065b524ac29871951e6f1ea023629ba2302
Leandro Amorim <leandroamorimpb@gmail.com>
2022-07-07 14:07:07 -0300

    Configuração das propriedades de conexão com o servidor SMTP Módulo 19 aula 19.3 :package:

--- envioemail/src/test/java/envioemail/email/AppTest.java
+++ envioemail/src/test/java/envioemail/email/AppTest.java
@@ -1,5 +1,7 @@
 package envioemail.email;
 
+import java.util.Properties;
+
 import org.junit.Test;
 
 public class AppTest {
@@ -7,6 +9,24 @@ public class AppTest {
 	@Test
 	public void testeEmail() {
 		
-		/*Olhe as configurações smtp do seu e-mail, cada tipo de e-mail de uma config*/
+		/*Olhe antes as configurações smtp do seu e-mail, cada tipo de e-mail de uma config*/
+		
+		/**
+		 * Configuração das propriedades
+		 */
+		Properties properties = new Properties();
+				 //put(chave, valor)
+		properties.put("mail.smtp.auth", "true");/*Autorização*/
+		properties.put("mail.smtp.starttls.enable", "true");/*Autenticação*/
+		properties.put("mail.smtp.host", "smtp.gmail.com");/*Servidor do gamil - Google*/
+		properties.put("mail.smtp.port", "587");/*Porta smtp do servidor*/
+		properties.put("mail.smtp.socketFactory.port", "465");/*Especifica a porta a ser utilizada para conexão pelo socket*/
+		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");/*Classe Socket de conexão ao SMTP*/
+		
+		
+		
+		
+		
+		
 	}
 }
